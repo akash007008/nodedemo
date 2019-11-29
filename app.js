fa7b0ae04fc9467907e3903a7578f29f92869bb3
akash007008 <agoyal706@gmail.com>
2019-11-30 00:34:05 +0530

    Working with express and ejs

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 var express=require('express');
 var app=express();
 
-app.get('/about',function(req,res){
-	res.send('<h1>About Us</h1>');
-});
-
 app.get('/about/:title',function(req,res){
-	res.send('<h1>'+req.params.title+'</h1>');
+	t=req.params.title;
+	res.render('index.ejs',{title:t});
 });
-
-app.get('/*',function(req,res){
-	res.send('<h1>Welcome To Express</h1>');
-});
-
-console.log('Server is running.')
+console.log('Server up');
 app.listen(3000);
